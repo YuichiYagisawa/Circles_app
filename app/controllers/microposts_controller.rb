f88125b0b7ce01bb5370793d6875c54b641af6c9
YuichiYagisawa <yuichi9915@gmail.com>
2019-06-14 17:15:55 +0900

    mictopost base

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,29 +6,53 @@ class MicropostsController < ApplicationController
   end
 
   def show
-    @micropost = Micropost.find(params[:id])
+    @micropost = Micropost.find_by(id: params[:id])
   end
 
   def new
     @micropost = Micropost.new
+
+  end
+
+  def edit
   end
 
   def create
     @micropost = Micropost.new(micropost_params)
     if @micropost.save
-      redirect_to microposts_path
+      redirect_to microposts_url
     else
       render 'new'
     end
   end
 
 
-  def destroy
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @micropost.update(micropost_params)
+        format.html { redirect_to @micropost, notice: 'micropost was successfully updated.' }
+        format.json { render :show, status: :ok, location: @micropost }
+      else
+        format.html { render :edit }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @micropost.destroy
+    respond_to do |format|
+      format.html { redirect_to microposts_url, notice: 'micropost was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
     def micropost_params
-      params.require(:micropost).permit(:content,:circle_id)
+      params.require(:micropost).permit(:content)
     end
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   # belongs_to :user
-  belongs_to :circle
+  # belongs_to :circle
   # default_scope -> {order(:created_at: :desc)}
-  validates :circle_id, presence: true
+  # validates :circle_id, presence: true
   # validates :user_id, presence: true
   validates :content, presence: true
 
